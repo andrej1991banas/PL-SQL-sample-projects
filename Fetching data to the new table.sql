6dba83193a0f51bc2b543a44c9056d85dfffe11b
andrej1991banas <limitless.lokca@gmail.com>
2025-01-28 19:54:40 +0100

    Add files via upload

---
+++ Fetching data to the new table.sql
@@ -0,0 +1,44 @@
+--TAKE PROGRAMMERS FROM EMPLOYEES TABLE, CREATE A NEW TABLE AND MOVE PROGRAMMERS 
+--INTO THE NEW TABLE, SHOW THEIR NAMES, USE LOOP PROCEDURE
+
+CREATE OR REPLACE PROCEDURE proc_cur_prog AS
+    CURSOR cur_prog IS
+    SELECT
+        *
+    FROM
+        employees
+    WHERE
+        job_title = 'Programmer';
+
+    rec_prog     employees%rowtype;
+    n_pocet_ludi NUMBER;
+BEGIN
+    OPEN cur_prog;
+    LOOP
+        FETCH cur_prog INTO rec_prog;
+        EXIT WHEN cur_prog%notfound;
+        INSERT INTO programmers (
+            fist_name,
+            last_name
+        ) VALUES (
+            rec_prog.first_name,
+            rec_prog.last_name
+        );
+
+        COMMIT;
+        dbms_output.put_line('Programator: '
+                             || rec_prog.first_name
+                             || ' '
+                             || rec_prog.last_name);
+
+    END LOOP;
+
+    n_pocet_ludi := cur_prog%rowcount;
+    dbms_output.put_line('celkovo sme sprocesovali riadkov:' || n_pocet_ludi);
+    CLOSE cur_prog;
+EXCEPTION
+    WHEN OTHERS THEN
+        dbms_output.put_line('MAME V PROCEDURE CHYBU' || sqlerrm);
+END;
+
+EXECUTE proc_cur_prog();
